7ad1812045945802ffff805ef00dab2d1a6d0fd2
Linh Vo <hallinhvo30112008@gmail.com>
2025-07-28 15:12:42 -0400

    Initial Commit

---
+++ BeautyVisa/BeautyVisaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeautyVisaApp.swift
+//  BeautyVisa
+//
+//  Created by Scholar on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct BeautyVisaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
