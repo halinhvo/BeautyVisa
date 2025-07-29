8ca02639c2d87dc5edd549009f91cf4014d334cb
Miley <cutegoldendragon@gmail.com>
2025-07-29 09:57:11 -0400

    added the regions button, secondview file, and the buttons

--- BeautyVisa/ContentView.swift
+++ BeautyVisa/ContentView.swift
@@ -1,19 +1,37 @@
-//
-//  ContentView.swift
-//  BeautyVisa
-//
-//  Created by Scholar on 7/28/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image("BeautyVisaLogo")
-                .resizable()
-                .frame(width: 200.0, height: 200.0)
-            Spacer()
+        NavigationStack {
+            VStack {
+                Image("BeautyVisaLogo")
+                    .resizable()
+                    .frame(width: 200.0, height: 200.0)
+                
+                Spacer()
+                
+                Text("Regions")
+                    .font(.system(size: 30))
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                    
+                
+                HStack(spacing: 20) {
+                    NavigationLink("Region 1", destination: SecondView())
+                    NavigationLink("Region 2", destination: SecondView())
+                    NavigationLink("Region 3", destination: SecondView())
+                }
+                .padding()
+                
+                HStack(spacing: 20) {
+                    NavigationLink("Region 4", destination: SecondView())
+                    NavigationLink("Region 5", destination: SecondView())
+                    NavigationLink("Region 6", destination: SecondView())
+                }
+                .padding()
+                
+                Spacer()
+            }
         }
     }
 }
---
+++ SecondView.swift
@@ -0,0 +1,26 @@
+import Foundation
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        
+        Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                
+                Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                        Button("Button") {
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                            Button("Button") {
+                                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
