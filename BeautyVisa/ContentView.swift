5f0102fa833fcee87b5a46979915a8fbb23c7175
Miley <cutegoldendragon@gmail.com>
2025-07-29 10:40:10 -0400

    added hello and ignored files

--- BeautyVisa/ContentView.swift
+++ BeautyVisa/ContentView.swift
@@ -5,6 +5,7 @@ struct ContentView: View {
             
         NavigationStack {
             VStack {
+                Text("Hello")
                 Image("BeautyVisaLogo")
                     .resizable()
                     .frame(width: 200.0, height: 200.0)
@@ -23,7 +24,7 @@ struct ContentView: View {
                 
                 HStack(spacing: 20) {
                     NavigationLink("Region 1", destination: SecondView())
-                    NavigationLink("Region 2", destination: SecondView())
+//                    NavigationLink("Region 2", destination: ThirdView())
                     NavigationLink("Region 3", destination: SecondView())
                 }
                 .padding()
